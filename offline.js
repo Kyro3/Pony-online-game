74b6679047cd1ed37d973dd075e552675f958a55
Kyro3 <61201387+Kyro3@users.noreply.github.com>
2022-04-21 12:46:14 +0300

    my goood game

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1650533904,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/object-sheet0.png",
+		"images/hero-sheet0.png",
+		"images/hero-sheet1.png",
+		"images/tiledbackground6.png",
+		"images/tiledbackground2.png",
+		"images/apple-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground7.png",
+		"images/tiledbackground8.png",
+		"images/kill-sheet0.png",
+		"images/spawn_point-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
